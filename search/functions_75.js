9fd6c8b4054d3a33aed40531686babd50a407dae
Tyler Heck <tyler.heck@gmail.com>
2013-06-06 11:37:22 -0400

    OBInt documentation added and updates to previous

---
+++ search/functions_75.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['unsignedvalue',['unsignedValue',['../OBInt__Private_8h.html#a05002f2a1b1acae0417a7c0d529917f8',1,'unsignedValue(const OBInt *a):&#160;OBInt.c'],['../OBInt_8c.html#a05002f2a1b1acae0417a7c0d529917f8',1,'unsignedValue(const OBInt *a):&#160;OBInt.c']]]
+];
--- search/typedefs_6f.js
+++ search/typedefs_6f.js
@@ -4,6 +4,7 @@ var searchData=
   ['obdequeiterator',['OBDequeIterator',['../OBDeque_8h.html#ac7349738514bb110104a6f5590dc1930',1,'OBDeque.h']]],
   ['obdequenode',['OBDequeNode',['../OBDeque__Private_8h.html#aad7d719be20af20a34cf519e88eddbbf',1,'OBDeque_Private.h']]],
   ['obhash_5ft',['obhash_t',['../offbrand_8h.html#a899693874716917329d244630f22e130',1,'offbrand.h']]],
+  ['obint',['OBInt',['../OBInt_8h.html#ae9eb07bb7af1019db4149ed13054f9a6',1,'OBInt.h']]],
   ['obj',['obj',['../offbrand_8h.html#a14f6160080cff474965b973219856f8f',1,'offbrand.h']]],
   ['obmap',['OBMap',['../OBMap_8h.html#a986601dc6bcce561c44f45e1ebcbf9de',1,'OBMap.h']]],
   ['obmappair',['OBMapPair',['../OBMap__Private_8h.html#ab33c81ebf5a49c971b19137ab87072fc',1,'OBMap_Private.h']]],
